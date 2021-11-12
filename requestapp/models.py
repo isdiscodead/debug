ba69d0fb4df15c53e35653d4faed2c3cd71d5d05
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-12 20:52:19 +0900

    add request model

--- requestapp/models.py
+++ requestapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Quest(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    # writer = models.ForeignKey
+    created_at = models.DateField(auto_now_add=True, null=True)
+
+    img = models.ImageField(upload_to='request/', null=True)
+    content = models.TextField(null=True)
+    location = models.CharField(max_length=200, null=False)
+
+    start_price = models.IntegerField(default=0)
+    end_price = models.IntegerField(default=0)
+    is_completed = models.BooleanField(default=False)
+
+
+
+    def __str__(self):
+        title
