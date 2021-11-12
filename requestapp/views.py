26e84abd5b8ffd86a60bf38e2c9d70998c0fb9b0
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-12 21:55:16 +0900

    conf templates

--- requestapp/views.py
+++ requestapp/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'requestapp/list.html')
+
+# class ArticleListView(ListView):
+#     model = Article
+#     context_object_name = 'article_List'
+#     template_name = 'articleapp/list.html'
+#     paginate_by = 8
