d9ad98f24e5fc5125ab9ec0e9de399c637453dda
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-12 22:33:31 +0900

    edit request list

--- requestapp/views.py
+++ requestapp/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from requestapp.models import Quest
+
 
 def home(request):
     return render(request, 'requestapp/list.html')
 
-# class ArticleListView(ListView):
-#     model = Article
-#     context_object_name = 'article_List'
-#     template_name = 'articleapp/list.html'
-#     paginate_by = 8
+
+class ArticleListView(ListView):
+    model = Quest
+    context_object_name = 'request_List'
+    template_name = 'requestapp/list.html'
+    paginate_by = 8
